b2bf947e8e8c39c5605dc304958bcb4ea534e9ad
RomaSamkov <Romansamkov.rs@gmail.com>
2022-08-30 17:58:29 +0300

    ad app

---
+++ app.js
@@ -0,0 +1,32 @@
+const fs = require('fs/promises');
+// const fs = require('fs').promises;
+
+const fileOperation = async (filePath, action = 'read', data = '') => {
+  switch (action) {
+    case 'read':
+      const text = await fs.readFile(filePath, 'utf-8');
+      console.log(text);
+      break;
+    case 'add':
+      await fs.appendFile(filePath, data);
+      break;
+    case 'replace':
+      await fs.writeFile(filePath, data);
+      break;
+
+    default:
+      break;
+  }
+};
+
+// fileOperation('files/file.txt');
+// fileOperation('files/file.txt', 'add', '\n Wow I dont know that \n do');
+fileOperation('files/file.txt', 'replace', '\nWow I dont know that\ndo');
+
+// fs.readFile('files/file.txt', 'utf-8')
+//   .then(data => {
+//     console.log(data);
+//     // const text = data.toString();
+//     // console.log(text);
+//   })
+//   .catch(error => console.log(error.message));
