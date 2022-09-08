659c29915b626fb6cb37f38cb995b871bd41e2c4
RomaSamkov <Romansamkov.rs@gmail.com>
2022-09-08 17:37:57 +0300

    Update app.js

--- express-example/app.js
+++ express-example/app.js
@@ -3,10 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  //   console.log(req.url);
-  //   console.log(req.method);
-  //   console.log(req.headers);
   res.send('<h2>Contacts Page</h2>');
+  res.json({ message: 'Hello Node.js' });
 });
 
 app.listen(3000, () => console.log('Server is Run...'));
